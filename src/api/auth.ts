5af3941df10b69a23f60ccb4972f32b15ed17d5e
TofuMango <yjlee0006@cu.ac.kr>
2026-04-07 18:09:58 +0900

    로그인 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/lib/supabase";
 
+// 회원가입 요청
 export async function signUp({
   email,
   password,
@@ -9,6 +10,23 @@ export async function signUp({
 }) {
   // supabase 서버에 요청
   const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+// 로그인 요청
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
