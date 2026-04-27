c0d045e3486d95b567497ead5b90abf9c0819089
TofuMango <yjlee0006@cu.ac.kr>
2026-04-27 18:56:59 +0900

    포스트 추가 기능 구현

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -3,9 +3,21 @@ import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
 import { usePostEditorModal } from "@/store/post-editor-modal";
 import { useEffect, useRef, useState } from "react";
+import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
+import { toast } from "sonner";
 
 export default function PostEditorModal() {
   const { isOpen, close } = usePostEditorModal();
+  const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
+    // 포스트 생성에 성공하면 modal close
+    onSuccess: () => {
+      close();
+    },
+    onError: (error) => {
+      toast.error("포스트 생성에 실패했습니다.");
+    },
+  });
+
   const [content, setContent] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -13,9 +25,11 @@ export default function PostEditorModal() {
     close();
   };
 
+  // 포스트 생성 요청
   const handleCreatePostClick = () => {
+    // content가 공백일 경우 그냥 return
     if (content.trim() === "") return;
-    
+    createPost(content);
   };
 
   useEffect(() => {
@@ -44,6 +58,7 @@ export default function PostEditorModal() {
       <DialogContent className="max-h-[90vh]">
         <DialogTitle>포스트 작성</DialogTitle>
         <textarea
+          disabled={isCreatePostPending}
           ref={textareaRef}
           value={content}
           onChange={(e) => setContent(e.target.value)}
@@ -54,7 +69,13 @@ export default function PostEditorModal() {
           <ImageIcon />
           이미지 추가
         </Button>
-        <Button className="cursor-pointer">저장</Button>
+        <Button
+          disabled={isCreatePostPending}
+          onClick={handleCreatePostClick}
+          className="cursor-pointer"
+        >
+          저장
+        </Button>
       </DialogContent>
     </Dialog>
   );
