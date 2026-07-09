64532a341dee87de55bdd6e4a1059b080da8cbb0
TofuMango <yjlee0006@cu.ac.kr>
2026-07-09 21:36:57 +0900

    PostEditorModal 생성/수정 모드 구분 및 상태 저장 추가

--- src/components/post/create-post-button.tsx
+++ src/components/post/create-post-button.tsx
@@ -1,13 +1,13 @@
-import { useOpenPostEditorModal } from "@/store/post-editor-modal";
+import { useOpenCreatePostModal } from "@/store/post-editor-modal";
 import { PlusCircleIcon } from "lucide-react";
 
 export default function CreatePostButton() {
-  // modal 여는 action의 open 함수 불러옴
-  const openPostEditorModal = useOpenPostEditorModal();
+  // 포스트 생성 모달 오픈 hook
+  const openCreatePostModal = useOpenCreatePostModal();
 
   return (
     <div
-      onClick={openPostEditorModal}
+      onClick={openCreatePostModal}
       className="bg-muted text-muted-foreground cursor-pointer rounded-xl px-6 py-4"
     >
       <div className="flex items-center justify-between">
